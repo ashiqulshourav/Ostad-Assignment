5bc8309ee19737d9b702bd27b18363a2fd7f8847
Ashiqul Islam <shouravnkt@gmail.com>
2023-06-18 23:39:14 +0600

    added 8 number answer

--- Module 3/app.js
+++ Module 3/app.js
@@ -108,12 +108,70 @@ getBrowserInfo();
 // 6. get user current location (latitude and longitude) using geolocation API.
 
 function getLocation(){
+    if(!navigator.geolocation) return;
     navigator.geolocation.getCurrentPosition((position)=>{
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
         
-        console.log(`6. User location latitute value is ${latitude} & longitude value is ${longitude}`);
+        console.log(`6. Your location latitute value is ${latitude} & longitude value is ${longitude}`);
+        // note: I didn't handle the error.
     });
 }
 
 getLocation();
+
+
+// 7. JS Common Events:
+console.log("7. Do a action from browser run by index.html file")
+    // a. get the codinates by onclick anywhere on the body
+
+    function getCoordinates (){
+        document.addEventListener('click', function(e){
+            const x = e.clientX; // getting x value
+            const y = e.clientY; // getting y value
+
+            alert(`7a. Your clicked Coordinates (x = ${x}, y = ${y})`);
+            console.log(`7a. Your clicked Coordinates (x = ${x}, y = ${y})`);
+        });
+    }
+
+    getCoordinates();
+
+    // b. user pressed key
+    function userPressedKey(){
+        document.addEventListener('keypress', (e)=>{
+            const pressedKey = e.key;
+            const pressedkeyCode = e.keyCode;
+            
+            alert(`7b. You pressed "${pressedKey}" which key code is "${pressedkeyCode}"`)
+            console.log(`7b. You pressed "${pressedKey}" which key code is "${pressedkeyCode}"`)
+        });
+    }
+
+    userPressedKey();
+
+    // c. image change by hover
+    const hoverImg = document.querySelector('.hover-to-change');
+    hoverImg.addEventListener('mouseover', function(e){
+        const src = `https://source.unsplash.com/featured/300x45${Math.floor(Math.random() * 9)}`;
+        e.target.removeAttribute('src');
+        e.target.setAttribute('src', src);
+
+        console.log(`7c. Changed image src is ${src}`)
+    })
+
+
+
+// 8. HTML DOM
+console.log("8. Please click the 'Click Me!' buttor for changed the text")
+const textChangeButton = document.querySelector('.textChangeButton');
+const changedText = document.querySelector('.changedText');
+
+
+function textChangeByButton(){
+    changedText.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, delectus. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae quas incidunt ipsum ipsam? Quidem?";
+
+    console.log(`8. Button clicked & changed text is ${changedText.innerText}`)
+}
+
+textChangeButton.addEventListener('click', textChangeByButton);
