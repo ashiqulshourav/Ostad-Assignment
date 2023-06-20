969d3604561a123f1568efcd7cc811579524c35b
Ashiqul Islam <shouravnkt@gmail.com>
2023-06-21 03:05:22 +0600

    restructured

--- Module 3/validation.js
+++ Module 3/validation.js
@@ -8,93 +8,118 @@ const confirmPassword = document.querySelector('.confirm-password');
 form.addEventListener('submit', formValidation);
 
 // Fire the name validation function
-name.addEventListener('keyup', namveValidation);
-name.addEventListener('paste', namveValidation);
+name.addEventListener('keyup', function(){
+    nameValidation(this);
+});
+
+name.addEventListener('paste', function(){
+    nameValidation(this);
+});
 
 
-// fire the email validation function
-email.addEventListener('keyup', validationEmail);   
-email.addEventListener('paste', validationEmail);
+
+// f.Fire the email validation function
+email.addEventListener('keyup', function(){
+    validationEmail(this);
+});
+
+email.addEventListener('paste', function(){
+    validationEmail(this);
+});
 
 
-// fire the password validation function
-newPassword.addEventListener('keyup', validatePassword);
-newPassword.addEventListener('paste', validatePassword);
+// // fire the password validation function
+newPassword.addEventListener('keyup', function(){
+    validatePassword(this);
+});
+
+newPassword.addEventListener('paste', function(){
+    validatePassword(this);
+});
 
 
-// fire the match password function
-confirmPassword.addEventListener('keyup', matchPassword);
-confirmPassword.addEventListener('paste', matchPassword);
+// // fire the match password function
+confirmPassword.addEventListener('keyup', function(){
+    matchPassword(this);
+});
+
+confirmPassword.addEventListener('paste', function(){
+    matchPassword(this);
+});
 
 
-function namveValidation(){
-    const valid = /^[A-Za-z]+$/.test(this.value);
+function nameValidation(element){
+    const valid = /^[A-Za-z]+$/.test(element.value);
     const inValidText = "name should only letters";
     const validText = "name is valid";
     
-    if(!valid){
-        showError(this, inValidText);
+    if(!valid || element.value.length < 1){
+        showError(element, inValidText);
     } else{
-        showSuccess(this, validText);
+        showSuccess(element, validText);
     }
 }
 
 
 
-function validationEmail() {
-    const valid = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/.test(this.value);
+function validationEmail(element) {
+    const valid = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/.test(element.value);
     const inValidText = "email is not valid";
     const validText = "email is valid";
 
-    if(!valid){
-        showError(this, inValidText);
+    if(!valid || element.value.length < 1){
+        showError(element, inValidText);
     } else{
-        showSuccess(this, validText);
+        showSuccess(element, validText);
     }
   }
 
 
-  function validatePassword() {
-    const valid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(this.value);
+  function validatePassword(element) {
+    const valid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(element.value);
     const inValidText = "Password should have a minimum length of 8 characters and contain at least one uppercase letter, one lowercase letter, and one digit.";
     const validText = "Password is valid";
 
-    if(!valid){
-        showError(this, inValidText);
+    if(!valid || element.value.length < 1){
+        showError(element, inValidText);
     } else{
-        showSuccess(this, validText);
+        showSuccess(element, validText);
     }
   }
 
 
-  function matchPassword(){
-    const valid = newPassword.value === this.value;
+  function matchPassword(element){
+    const valid = newPassword.value === element.value;
     const inValidText = "Password did not match!";
     const validText = "Password is matched!";
 
-    if(!valid){
-        showError(this, inValidText);
+    if(!valid || element.value.length < 1){
+        showError(element, inValidText);
     } else{
-        showSuccess(this, validText);
+        showSuccess(element, validText);
     }
   }
 
 
-  function showError(element, text){
+function showError(element, text){
+    // console.log(element.value, element.value.length, "ShowError")
     element.classList.remove('border-green-500');
     element.classList.add('border-red-500');
     element.nextElementSibling.textContent = text;
     element.nextElementSibling.classList.add('text-red-500');
+    element.nextElementSibling.classList.remove('text-green-500');
     element.nextElementSibling.classList.remove('hidden');
 }
 
 function showSuccess(element, text){
+    // console.log(element.value, element.value.length, "ShowSuccess")
+    if(element.classList.contains('border-red-500')){
+        element.classList.add('border-green-500');
+    }
     element.classList.remove('border-red-500');
-    element.classList.add('border-green-500');
     element.nextElementSibling.textContent = text;
     element.nextElementSibling.classList.remove('text-red-500');
     element.nextElementSibling.classList.add('text-green-500');
-    element.nextElementSibling.classList.remove('hidden');
 }
 
 
@@ -106,21 +131,30 @@ function formValidation (e){
     isEmpty(newPassword);
     isEmpty(confirmPassword);
 
-    
-
     // check empty value
-    function isEmpty(element){
-        if(element.value == "" || element.value.length < 1){
+    function isEmpty(element){  
+        if(element.value == "" || element.value.length < 1 ){
             element.classList.add('border-red-500');
+            element.classList.remove('border-green-500');
             element.nextElementSibling.textContent = "field can not be empty";
             element.nextElementSibling.classList.add('text-red-500');
+            element.nextElementSibling.classList.remove('text-green-500');
             element.nextElementSibling.classList.remove('hidden');
         } else{
             element.classList.remove('border-red-500');
             element.classList.remove('border-green-500');
             element.nextElementSibling.textContent = "";
             element.nextElementSibling.classList.remove('text-red-500');
+            element.nextElementSibling.classList.remove('text-green-500');
             element.nextElementSibling.classList.add('hidden');
+
+            nameValidation(name);
+            validationEmail(email);
+            validatePassword(newPassword);
+            matchPassword(confirmPassword);
         }
     }
+
+    
+
 }
