41b0de2b3e42fc816404591371275eded79acbb2
Ashiqul Islam <shouravnkt@gmail.com>
2023-07-05 01:53:24 +0600

    restructured

--- Module-4/assets/js/app.js
+++ Module-4/assets/js/app.js
@@ -1,4 +1,4 @@
-import { addToCart } from './cart.js';
+import { addToCart, getCartItems } from './cart.js';
 import products from "./product.js";
 
 const cartPopup = document.querySelector('#cartPopup');
@@ -34,7 +34,7 @@ products.map((item)=>{
     <span class="product-img max-w-[150px] max-h-[200px]">
     <img class="max-h-[200px]" src="${item.image}" alt="${item.description}" />
     </span>
-            <span class="product-price">${item.price}$</span>
+            <span class="product-price">$${item.price}</span>
             <button
               class="add-to-cart bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               >
@@ -43,10 +43,7 @@ products.map((item)=>{
               </div>
               </div>
               `;
-            })
-            
-            
-            
+            });      
             
 const addToCartBtns = document.querySelectorAll('.add-to-cart');
 
@@ -59,7 +56,106 @@ addToCartBtns.forEach((btn)=>{
         
         if (product && quantity > 0) {
             addToCart(product, quantity);
+            displayCartItems();
           }
 
     });
-})
+});
+
+
+function displayCartItems (){
+    const cartBody = document.querySelector('.cart-body');
+    const cartItems = getCartItems ();
+
+    cartItems.map((item)=>{
+        cartBody.innerHTML += `
+            <div class="item flex justify-between items-center">
+            <span class="item-id">${item.product.id}</span>
+            <span class="item-name">${item.product.title.slice(0, 20)}</span>
+            <div class="product-img max-w-[80px] max-h-[100px]">
+            <img
+                class="max-h-[100px]"
+                src="${item.product.image}"
+                alt="Hello"
+            />
+            </div>
+            <span class="item-price">$${item.product.price}</span>
+            <div class="quantity-box flex justify-between items-center">
+            <button class="quan-dec px-2 py-1 bg-red-400 text-white">
+                -
+            </button>
+            <span class="item-quantity px-4">1</span>
+            <button class="quan-inc px-2 py-1 bg-gray-700 text-white">
+                +
+            </button>
+            </div>
+            <span class="item-total-price">$${item.product.price}</span>
+            <button
+            class="remove-item p-2 px-5 bg-red-700 text-white rounded-lg"
+            >
+            remove
+            </button>
+        </div>`;
+    });
+    
+    sumOfAllItemPrice();
+}
+
+
+const quantityIncBtns = document.querySelectorAll('.quan-inc');
+const quantityDecBtns = document.querySelectorAll('.quan-dec');
+
+
+// Quantity Increment
+quantityIncBtns.forEach((btn)=>{
+    btn.addEventListener('click', function(){
+        let itemQuantity = this.parentNode.querySelector('.item-quantity');
+        let quantity = itemQuantity.innerText;
+        let item = this.parentNode.parentNode.querySelector('.item-total-price');
+        let itemPrice = parseInt(this.parentNode.parentNode.querySelector('.item-price').innerText.replace("$",""));
+        if(quantity > 0){
+            quantity++;
+            itemQuantity.innerText = quantity;
+            individualItemPriceUpdate(quantity, itemPrice, item)
+        }
+    });    
+});
+
+// Quantity Decreament
+quantityDecBtns.forEach((btn)=>{
+    btn.addEventListener('click', function(){
+        let itemQuantity = this.parentNode.querySelector('.item-quantity');
+        let quantity = itemQuantity.innerText;
+        let item = this.parentNode.parentNode.querySelector('.item-total-price')
+        let itemPrice = parseInt(this.parentNode.parentNode.querySelector('.item-price').innerText.replace("$",""));
+        if(quantity > 1){
+            quantity--;
+            itemQuantity.innerText = quantity;
+            individualItemPriceUpdate(quantity, itemPrice, item);
+        }
+    })
+});
+
+
+function individualItemPriceUpdate (quantity, price, item){
+    item.innerText = "$" + quantity * price;
+    sumOfAllItemPrice();
+}
+
+
+function sumOfAllItemPrice(){
+    const items = document.querySelectorAll('.item-total-price');
+    let grandTotal = document.querySelector('.grand-total');
+    let sum = 0;
+
+    [...items].map((item)=>{
+        sum += Number(item.innerText.replace("$",""));
+    });
+
+    grandTotal.innerText = `$${sum}`;
+}
+
+sumOfAllItemPrice();
+
+
+
--- Module-4/assets/js/cart.js
+++ Module-4/assets/js/cart.js
@@ -2,13 +2,13 @@ let cartItems = [];
 
 export function addToCart(product, quantity = 1) {
   const cartItem = cartItems.find(item => item.product.id === product.id);
-
   if (cartItem) {
     cartItem.quantity += quantity;
   } else {
     cartItems.push({ product, quantity });
   }
 }
+
 
 export function getCartItems() {
   return cartItems;
