e4ab9201ef8eaa39ca8e52aeaf019a6a2b4ae34a
Ashiqul Islam <59530063+ashiqulshourav@users.noreply.github.com>
2023-07-05 12:48:33 +0600

    restructured

--- Module-4/assets/js/app.js
+++ Module-4/assets/js/app.js
@@ -1,11 +1,11 @@
-import { addToCart, getCartItems } from './cart.js';
+import { addToCart, clearCart, getCartItems } from './cart.js';
 import products from "./product.js";
 
 const cartPopup = document.querySelector('#cartPopup');
 const cartBtn = document.querySelector('.header-cart');
 const popupCloseBtn = document.querySelector('.popup-close');
 
-const notification = document.querySelector('.notification');
+let notification = document.querySelector('.notification');
 
 
 
@@ -14,18 +14,20 @@ popupCloseBtn.addEventListener('click', closePopup);
 cartBtn.addEventListener('click', openPopup);
 
 // open popup
-function closePopup (){
+function closePopup() {
     cartPopup.classList.add('hidden');
+    getNotification();
 }
 
 // close popup
-function openPopup (){
+function openPopup() {
     cartPopup.classList.remove('hidden');
+    displayCartItems();
 }
 
 
 // Show products 
-products.map((item)=>{
+products.map((item) => {
     document.querySelector('.items-inner').innerHTML += `
     <div class="item">
     <div class="item-inner flex justify-between items-center pt-8 w-[90%] max-w-5xl m-auto">
@@ -43,38 +45,35 @@ products.map((item)=>{
               </div>
               </div>
               `;
-            });      
-            
+});
+
 const addToCartBtns = document.querySelectorAll('.add-to-cart');
 
-addToCartBtns.forEach((btn)=>{
-    btn.addEventListener('click', (e) =>{
+addToCartBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
         const productId = Number(e.target.parentNode.querySelector('.item-num').innerText);
         const product = products.find(p => p.id === productId);
 
-        console.log(product, "Product")
         const quantity = 1;
 
-        
         if (product && quantity > 0) {
             addToCart(product, quantity);
-            displayCartItems();
-          }
-
+            getNotification();
+        }
     });
 });
 
 
-function displayCartItems (){
-    const cartBody = document.querySelector('.cart-body');
-    const cartItems = getCartItems ();
+function displayCartItems() {
+    const cartBody = document.querySelector('.cart-body-content');
+    const cartItems = getCartItems();
 
-    console.log(cartItems, "cartItems in DisplayCartItems");
+    // reset cartBody HTML
+    cartBody.innerHTML = "";
 
-    cartItems.forEach((item)=>{
-        console.log(item, "Cart item item");
+    cartItems.forEach((item) => {
         cartBody.innerHTML += `
-            <div class="item flex justify-between items-center">
+            <div class="item flex justify-between items-center py-2">
             <span class="item-id">${item.product.id}</span>
             <span class="item-name">${item.product.title.slice(0, 20)}</span>
             <div class="product-img max-w-[80px] max-h-[100px]">
@@ -94,7 +93,7 @@ function displayCartItems (){
                 +
             </button>
             </div>
-            <span class="item-total-price">$${item.product.price}</span>
+            <span class="item-total-price">$${item.product.price * item.quantity}</span>
             <button
             class="remove-item p-2 px-5 bg-red-700 text-white rounded-lg"
             >
@@ -102,11 +101,106 @@ function displayCartItems (){
             </button>
         </div>`;
     });
-    
-    // sumOfAllItemPrice();
+
+
+    sumOfAllItemPrice();
+
+    const quantityIncBtns = document.querySelectorAll('.quan-inc');
+    const quantityDecBtns = document.querySelectorAll('.quan-dec');
+    const removeItemBtns = document.querySelectorAll('.remove-item');
+    const clearCartBtn = document.querySelectorAll('.clear-cart');
+
+
+    // Quantity Increment
+    quantityIncBtns.forEach((btn) => {
+        btn.addEventListener('click', function() {
+            const productId = Number(this.parentNode.parentNode.querySelector('.item-id').innerText);
+            const cartItem = cartItems.find(item => item.product.id === productId);
+
+            let itemQuantity = this.parentNode.querySelector('.item-quantity');
+            let item = this.parentNode.parentNode.querySelector('.item-total-price');
+            let itemPrice = cartItem.product.price.toFixed(2);
+
+
+            if (cartItem.quantity > 0) {
+                cartItem.quantity++;
+                itemQuantity.innerText = cartItem.quantity;
+                individualItemPriceUpdate(cartItem.quantity, itemPrice, item);
+            }
+        });
+    });
+
+
+    // Quantity Decrement
+    quantityDecBtns.forEach((btn) => {
+        btn.addEventListener('click', function() {
+            const productId = Number(this.parentNode.parentNode.querySelector('.item-id').innerText);
+            const cartItem = cartItems.find(item => item.product.id === productId);
+
+            let itemQuantity = this.parentNode.querySelector('.item-quantity');
+            let item = this.parentNode.parentNode.querySelector('.item-total-price');
+            let itemPrice = cartItem.product.price.toFixed(2);
+
+
+            if (cartItem.quantity > 1) {
+                cartItem.quantity--;
+                itemQuantity.innerText = cartItem.quantity;
+                individualItemPriceUpdate(cartItem.quantity, itemPrice, item);
+            }
+        });
+    });
+
+    // remove individual item
+    removeItemBtns.forEach((btn) => {
+        btn.addEventListener('click', function() {
+            const productId = Number(this.parentNode.querySelector('.item-id').innerText);
+            const cartItem = cartItems.find(item => item.product.id === productId);
+
+            // Remove the item from the cartItems array
+            const itemIndex = cartItems.findIndex(item => item.product.id === productId);
+            cartItems.splice(itemIndex, 1);
+
+            // Regenerate the cart body HTML
+            displayCartItems();
+        });
+    });
+
+    // clear cart
+    clearCartBtn.forEach((btn) => {
+        btn.addEventListener('click', function() {
+            clearCart();
+            displayCartItems();
+        });
+    });
 }
 
 
 
+function getNotification() {
+    const items = getCartItems();
+    if (items.length === 0) {
+        notification.classList.remove('active');
+    } else {
+        notification.classList.add('active');
+        notification.innerText = items.length;
+    }
+}
 
 
+function individualItemPriceUpdate(quantity, price, item) {
+    let total = quantity * price
+    item.innerText = "$" + total.toFixed(2);
+    sumOfAllItemPrice();
+}
+
+
+
+function sumOfAllItemPrice() {
+    const items = document.querySelectorAll('.item-total-price');
+    let grandTotal = document.querySelector('.grand-total');
+    let sum = [...items].reduce((acc, item) => {
+        return acc + Number(item.innerText.replace('$', ""));
+    }, 0);
+
+    grandTotal.innerText = `$${sum.toFixed(2)}`;
+}
--- Module-4/assets/js/cart.js
+++ Module-4/assets/js/cart.js
@@ -1,86 +1,21 @@
 let cartItems = [];
 
 export function addToCart(product, quantity = 1) {
-  const cartItem = cartItems.find(item => item.product.id === product.id);
-  console.log(cartItem, "cartItem match")
-  if (cartItem) {
-    cartItem.quantity += quantity;
-  } else {
-    cartItems.push({ product, quantity });
-  }
-  console.log(cartItems, "CartItems array")
+    const cartItem = cartItems.find(item => item.product.id === product.id);
+    //   console.log(cartItem, "cartItem match")
+    if (cartItem) {
+        cartItem.quantity += quantity;
+    } else {
+        cartItems.push({ product, quantity });
+    }
+    //   console.log(cartItems, "CartItems array")
 }
 
 
 export function getCartItems() {
-  return cartItems;
+    return cartItems;
 }
 
 export function clearCart() {
-  cartItems = [];
+    cartItems = [];
 }
-
-
-
-const quantityIncBtns = document.querySelectorAll('.quan-inc');
-    const quantityDecBtns = document.querySelectorAll('.quan-dec');
-    const removeItemBtns = document.querySelectorAll('.remove-item');
-
-    [...removeItemBtns].map((btn)=>{
-        btn.addEventListener('click', function(){
-            let id = this.parentNode;
-            console.log(id)
-        });
-    });
-
-
-    // Quantity Increment
-    quantityIncBtns.forEach((btn)=>{
-        btn.addEventListener('click', function(){
-            let itemQuantity = this.parentNode.querySelector('.item-quantity');
-            let quantity = itemQuantity.innerText;
-            let item = this.parentNode.parentNode.querySelector('.item-total-price');
-            let itemPrice = Number(this.parentNode.parentNode.querySelector('.item-price').innerText.replace("$",""));
-            if(quantity > 0){
-                quantity++;
-                itemQuantity.innerText = quantity;
-                individualItemPriceUpdate(quantity, itemPrice, item)
-            }
-        });    
-    });
-
-    // Quantity Decreament
-    quantityDecBtns.forEach((btn)=>{
-        btn.addEventListener('click', function(){
-            let itemQuantity = this.parentNode.querySelector('.item-quantity');
-            let quantity = itemQuantity.innerText;
-            let item = this.parentNode.parentNode.querySelector('.item-total-price')
-            let itemPrice = Number(this.parentNode.parentNode.querySelector('.item-price').innerText.replace("$",""));
-            if(quantity > 1){
-                quantity--;
-                itemQuantity.innerText = quantity;
-                individualItemPriceUpdate(quantity, itemPrice, item);
-            }
-        })
-    });
-
-
-    function individualItemPriceUpdate (quantity, price, item){
-        item.innerText = "$" + quantity * price;
-        sumOfAllItemPrice();
-    }
-
-
-    function sumOfAllItemPrice(){
-        const items = document.querySelectorAll('.item-total-price');
-        let grandTotal = document.querySelector('.grand-total');
-        let sum = 0;
-
-        [...items].map((item)=>{
-            sum += Number(item.innerText.replace("$",""));
-        });
-
-        grandTotal.innerText = `$${sum}`;
-    }
-
-    sumOfAllItemPrice();
