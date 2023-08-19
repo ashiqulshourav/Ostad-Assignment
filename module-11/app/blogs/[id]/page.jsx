4319b6498021063b2fa90b125e34151ae83a7e32
Ashiqul Islam <shouravnkt@gmail.com>
2023-08-19 21:31:26 +0600

    fixed post posts

--- module-11/app/blogs/[id]/page.jsx
+++ module-11/app/blogs/[id]/page.jsx
@@ -1,22 +1,16 @@
 "use client";
 
 import blogPosts from "@/data/blogData.json";
-import { useEffect, useState } from "react";
 
 export default function Post({ params }) {
   const id = params.id;
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    const postData = blogPosts.find((post) => post.id === Number(id));
-    setPost(postData);
-  }, [id]);
+  const postData = blogPosts.find((post) => post.id === Number(id));
 
   return (
     <>
-      <div key={post.id}>
-        <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
-        <p className="text-lg">{post.content}</p>
+      <div key={postData.id}>
+        <h1 className="text-4xl font-bold mb-8">{postData.title}</h1>
+        <p className="text-lg">{postData.content}</p>
       </div>
     </>
   );
